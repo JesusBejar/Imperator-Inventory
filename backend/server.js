41ab6750f65a1daee9eaa78562cf2fa370ddd3f3
Jesus Bejar <israelbejarii@gmail.com>
2024-12-23 21:41:49 -0700

    added route & env (dotenv)

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,12 @@ const express = require('express')
 // variable that contains express app
 const app = express()
 
+// route handlers
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome to my project'})
+
+})
+
 // listener
 app.listen(4000, () => {
     console.log('listening on port 4000')
