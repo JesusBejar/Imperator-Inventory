83c4e2c134f874d139a02e88b017c2add308365f
Jesus Bejar <israelbejarii@gmail.com>
2024-12-25 16:13:03 -0700

    added simple middleware

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,13 @@ const express = require('express')
 // variable that contains express app
 const app = express()
 
+// middleware
+// invoke next so the next function will run
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
 // route handlers
 app.get('/', (req, res) => {
     res.json({mssg: 'Welcome to my project'})
