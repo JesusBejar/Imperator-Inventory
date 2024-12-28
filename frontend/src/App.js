4aaba1f5d975f4bfa461a779d781d5fa3db5377e
Jesus Bejar <israelbejarii@gmail.com>
2024-12-28 10:57:13 -0700

    made home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+// pages and components
+import Home from '../pages/home'
+
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+      <div className="pages">
+        <Routes>
+          <Route>
+            path='/'
+            element={<Home />}
+          </Route>
+        </Routes>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
