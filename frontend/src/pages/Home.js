f043b8e0382cf93f874d523c999e6a3e03d7ba03
Jesus Bejar <israelbejarii@gmail.com>
2024-12-30 18:43:31 -0700

    added useEffect & useState hooks

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react"
+
 const Home = () => {
+    const [divisions, setDivisions] = useState(null)
+
+    useEffect(() => {
+        const fetchDivisions = async () => {
+            const response = await fetch('http://localhost:4000/api/divisions')
+            const json = await response.json()
+
+            if (response.ok) {
+                setDivisions(json)
+            }
+        }
+
+        fetchDivisions()
+    }, [])
+
     return (
         <div className="home">
-            <h2>
-                Home
-            </h2>
+            <div className='divisions'>
+                {divisions && divisions.map(() => (
+                    <p key={divisions._id}>{division.title}</p>
+                ))}
+            </div>
         </div>
     )
 }
