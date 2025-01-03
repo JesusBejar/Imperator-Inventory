19b691f36a0cbc8f9f0e077145195f810c5bf56e
Jesus Bejar <israelbejarii@gmail.com>
2025-01-02 20:54:15 -0700

    added fetch request

--- frontend/src/components/DivisionForm.js
+++ frontend/src/components/DivisionForm.js
@@ -1,6 +1,25 @@
 import { useState } from "react"
 
 const divisionForm = () => {
+    const [dTitle, setdTitle] = useState('')
+    const [iCount, setiCount] = useState('')
+    const [hCount, sethCount] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const division = {dTitle, iCount, hCount}
+
+        const response = await fetch('/api/divisions', {
+            method: "POST", 
+            body: JSON.stringify(division), 
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+        const json = await response.json()
+    }
 
     return (
         <form className="create" onSubmit={handleSubmit}>
