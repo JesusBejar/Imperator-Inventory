2fd195319fcddbe8fbc8ea28d7f942ea72543019
Jesus Bejar <israelbejarii@gmail.com>
2025-01-06 09:33:02 -0700

    exported everything

--- frontend/src/context/DivisionContext.js
+++ frontend/src/context/DivisionContext.js
@@ -1,15 +1,15 @@
 import { createContext, useReducer } from 'react'
 
-const DivisionsContext = createContext()
+export const DivisionsContext = createContext()
 
-const divisionsReducer = (state, action) => {
+export const divisionsReducer = (state, action) => {
     switch (action.type) {
-        
+
     }
 }
 
 
-const DivisionsContextProvider = ({ children }) => {
+export const DivisionsContextProvider = ({ children }) => {
     const [ state, dispatch] = useReducer(divisionsReducer, {
         divisions: null
     })
